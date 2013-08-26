635c16403cac96bc3545f222e2c82f0804db1b48
rarck591 <reyarceno@gmail.com>
2013-08-26 16:48:04 -0500

    Initial Commit

---
+++ NameLength.rb
@@ -0,0 +1,26 @@
+puts 'Please enter your first name.'
+firstname = gets.chomp
+puts ''
+puts 'Hi, ' + firstname + '!  What a shitty first name!'
+puts ''
+puts 'I hope you have a better middle name.'
+puts ''
+puts 'What\'s your middle name, ' + firstname + '?'
+midname = gets.chomp
+puts ''
+puts 'Jesus Christ!  That\'s not much better.'
+puts ''
+puts 'So what\'s your last name then, ' + firstname + ' ' + midname + '?'
+lastname = gets.chomp
+puts ''
+puts firstname + ' ' + midname + ' ' + lastname + '?!'
+puts ''
+puts 'That must be the worst name I\'ve ever heard.'
+puts ''
+
+puts 'Anyway.'
+puts 'If there are ' + firstname.length.to_s + ' characters in your first name,'
+puts midname.length.to_s + ' in your middle name,'
+puts 'and ' + lastname.length.to_s + ' in your last name,'
+puts 'then there is a total of, ' + firstname.length.to_s + midname.length.to_s + lastname.length.to_s + ' characters' 
+puts 'in your name, ' + firstname + ' ' + midname + ' ' + lastname + '.'
