22fb2eae47134744d951650b3f20fb391c32715b
rarck591 <reyarceno@gmail.com>
2013-08-26 18:06:35 -0500

    Added Code for Angry Boss and Table of Contents

--- AngryBoss.rb
+++ AngryBoss.rb
@@ -1,5 +1,9 @@
 puts '"Hey, Crappy Employee.  What can I do ya for?"'
+
 puts '(Please state your business.)'
+
 answer = gets.chomp
+
 puts ''
+
 puts 'WHATCHOU MEAN "' + answer.upcase + '?!"  GET TO STEPPIN\'!'
--- TableContents.rb
+++ TableContents.rb
@@ -1,6 +1,9 @@
 title = 'Table of Contents'.center(50)
+
 puts title
+
 puts ''
+
 puts 'Chapter 1:  Getting Started'.ljust(33) + 'page 1'.rjust(15)
 
 puts 'Chapter 2:  Numbers'.ljust(33) + 'page 9'.rjust(15)
