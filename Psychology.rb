7b564268245ce9f4ff7b5c401d2b758c1ff314ba
rarck591 <reyarceno@gmail.com>
2013-09-02 12:11:05 -0500

    Initial Commit

---
+++ Psychology.rb
@@ -0,0 +1,66 @@
+puts 'Hello!  Thank you for your participation in this experiment.'
+puts 'This experiment deals with your opinions on Mexican food.'
+puts 'Please answer each question honestly with a "yes" or "no".'
+
+puts ''
+
+def ask question
+
+  while true
+
+    puts question
+
+    reply = gets.chomp.downcase
+
+    if reply == 'yes'
+
+      return true
+
+    end
+
+    if reply == 'no'
+
+      return false
+
+    end
+
+    # We will begin looping question
+
+    puts 'Please answere "yes" or "no".'
+
+  end
+
+end
+
+likes = ask 'Do you like tacos?'
+
+puts likes
+
+likes = ask 'Do you like burritos?'
+
+puts likes
+
+wet_bed = ask 'Do you wet the bad?'
+
+puts wet_bed
+
+likes = ask 'Do you like dulce de leche?'
+
+puts likes
+
+puts 'DEBRIEFING:'
+
+puts ''
+
+puts 'Thank you again for your participation!'
+
+puts ''
+
+puts 'I\'m sorry to deceive you, but this experiment' 
+puts 'was really about bed wetting.'
+
+puts ''
+
+puts 'We used the questions about Mexican food to throw you off.'
+
+puts ''
