3abece37511053f827f6880c3f0789d951757a6d
rarck591 <reyarceno@gmail.com>
2013-09-02 10:02:20 -0500

    Initial Commit

---
+++ ArraySort.rb
@@ -0,0 +1,21 @@
+puts 'Type in some words, and I will sort them alphabetically:'
+
+words = []
+
+while true
+  
+  word = gets.chomp
+
+  if word == ''
+   
+    break
+
+  end
+
+  words.push word
+end
+
+puts 'Here are your alphabetically sorted words:'
+puts ''
+puts words.sort
+puts ''
